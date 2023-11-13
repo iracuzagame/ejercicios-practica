e00509087c94c70a9fd49c4df9c9a6f5b5d204ad
Iracuza <aj_serra@hotmail.com>
2023-11-12 20:49:15 -0400

    new file:   creador_qr/linkedinQR.png

--- creador_qr/generadorQR.py
+++ creador_qr/generadorQR.py
@@ -2,8 +2,8 @@ import qrcode
 
 qr= qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=7, border=2)
 
-qr.add_data('https://github.com/iracuzagame')
+qr.add_data('www.linkedin.com/in/abel-serra-zambrano-59050a260')
 qr.make(fit=True)
 
 img = qr.make_image(fill_color='black', back_color='white')
-img.save('GitHubQR.png')
+img.save('linkedinQR.png')
